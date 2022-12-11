57f40aafd8937d4ff15367b5e619c0dd42839b7f
20191663 <ihs20191663@gmail.com>
2022-12-12 04:30:56 +0900

    동영상 배치&문구수정&상담소버튼 구현 완료

--- frontend/src/pages/Analyze/Analyze.js
+++ frontend/src/pages/Analyze/Analyze.js
@@ -9,7 +9,7 @@ const Analyze = () => {
     const [loading, setLoading] = useState(true);
     const [emote, setEmote] = useState("");
     const [score, setScore] = useState("");
-    const [youtube, setYoutube] = useState("");
+    const [yt, setYt] = useState("");
     
     const location = useLocation();
     console.log(location);
@@ -37,10 +37,11 @@ const Analyze = () => {
     useEffect(() => {
         SendNodejs({ text: location.state.data })
             .then((res)=>{
-                console.log(res);
                 setEmote(res.emotion.emotion);
                 setScore(res.emotion.score);
-                setYoutube(res.youtube[0].id.videoId);
+                for (let i = 0; i < 9; i++) {
+                    setYt(yt => [...yt, res.youtube[i].id.videoId]);
+                }
             })
             .catch((e) => {
                 console.log(e);
@@ -52,16 +53,23 @@ const Analyze = () => {
             <Header />
             {loading ? <Loading /> : <>
             <div>
-                <h1>분석 결과</h1><br/>
-                <h3>당신의 현재 감정은 <h1>{emote}</h1> 입니다.</h3><br/>
-                <h3>당신의 감정 점수는 <h1>{score}</h1> 입니다.</h3><br/>
+                <h1><br/>당신의 기분에 어울리는 컨텐츠를 준비해보았어요.</h1><br/>
             </div>
+            {(emote === "상처" || emote === "슬픔") && score >= 0.9 ?
+            <button
+                onClick={()=>window.open('./counsel', '_blank')}
+                class="button button_primary"
+                style={{ textDecoration: "none" }}
+                >
+                심리상담소 찾아보기
+            </button>:null}
             <div class="videoWrapper">
-                <iframe src={`https://www.youtube.com/embed/${youtube}?controls=0&autoplay=1&autoplay=1&loop=1&playlist=${youtube}`}
-                    style={{ width: "50%", height: "50%"}}
-                    allow="autoplay; fullscreen"
-                    title='감정 기반 추천 영상'>
+                {yt.map((item, index) => (
+                <iframe src={`https://www.youtube.com/embed/${item}?controls=0&autoplay=1&autoplay=1&loop=1&playlist=${item}`}
+                    allow="fullscreen"
+                    title={"감정 분석 영상 "+index}>
                 </iframe>
+                ))}
             </div>
             </>
             }
